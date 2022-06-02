59e6eadab93c0652fa7ad1d6cd031dc8f749a1ca
Sean Hatton <41756439+seanhatton@users.noreply.github.com>
2022-06-02 15:39:52 -0700

    Create mod2mrc.bash

---
+++ mod2mrc.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Model to MRC (mod2mrc)
+# Takes an MRC and matching/aligned IMOD model file 
+# to produce a binary MRC of the model
+# https://bio3d.colorado.edu/imod/
+
+input_mrc="input_image.mrc"
+input_mod="manually_segmented_objects.mod"
+declare -a object_list
+# List the object names in numerical order (object 1 first)
+object_list=(membrane synapse mitochondia vesicle)
+
+for i in "${!object_list[@]}"; do
+    obj_num="$(($i + 1))"
+    printf "Processing object %s labelled as %s\n" "$obj_num" "${object_list[$i]}"
+    imodextract $obj_num $input_mod $i.mod
+    imodmesh -C $i.mod
+    imodmop -mask 1 $i.mod $input_mrc $i.mrc 
+done
