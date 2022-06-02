173710d5e0ee72e33c8eafaf46398c1e37ce2c7b
Sean Hatton <41756439+seanhatton@users.noreply.github.com>
2022-06-02 16:03:18 -0700

    Update mod2mrc.bash

--- mod2mrc.bash
+++ mod2mrc.bash
@@ -13,7 +13,8 @@ object_list=(membrane synapse mitochondia vesicle)
 for i in "${!object_list[@]}"; do
     obj_num="$(($i + 1))"
     printf "Processing object %s labelled as %s\n" "$obj_num" "${object_list[$i]}"
-    imodextract $obj_num $input_mod $i.mod
-    imodmesh -C $i.mod
-    imodmop -mask 1 $i.mod $input_mrc $i.mrc 
+    imodextract $obj_num $input_mod ${object_list[$i]}.mod
+    imodmesh -C ${object_list[$i]}.mod
+    imodmop -mask 1 $i.mod $input_mrc ${object_list[$i]}.mrc 
 done
+rm *.mod~
